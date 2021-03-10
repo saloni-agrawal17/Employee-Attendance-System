140c08130717a9c18893c859687cf3a49202268d
saloni <agrawalsaloni52@gmail.com>
2021-03-10 16:34:30 +0530

    changes to settings"

--- attendanceSystem/attendanceSystem/settings.py
+++ attendanceSystem/attendanceSystem/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': 'attendancesystem',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': '127.0.0.1',
+        'HOST': 'agrawalsaloni17.mysql.pythonanywhere-services.com',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
