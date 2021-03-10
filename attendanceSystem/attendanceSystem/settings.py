dfae3328a9e9342151d6f2dfed7862c8dd437adb
saloni <agrawalsaloni52@gmail.com>
2021-03-10 16:13:30 +0530

    changes to settings

--- attendanceSystem/attendanceSystem/settings.py
+++ attendanceSystem/attendanceSystem/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
         'NAME': 'attendancesystem',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
